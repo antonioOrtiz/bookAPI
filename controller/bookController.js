0d8f2afab906aab62ad8a2d1ec2a6919a921435d
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2016-10-24 09:00:55 -0400

    bookController now home for post and get function

---
+++ controller/bookController.js
@@ -0,0 +1,31 @@
+var bookController = function bkCtrlHndlr(Book) {
+    // body... 
+    var post = function(req, res) {
+            var book = new Book(req.body);
+
+            book.save();
+
+            res.status(201).send(book);
+        },
+        get = function(req, res) {
+            /* body... */
+            var query = {};
+            if (req.query.genre) {
+                query.genre = req.query.genre;
+            }
+            Book.find(query, function(err, books) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    // statement;
+                    res.json(books);
+                }
+            });
+        };
+    return {
+        post: post,
+        get: get
+    };
+};
+
+module.exports = bookController;
